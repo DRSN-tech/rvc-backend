63e3cc77d4d9f3cfc735c15338a05d445982f407
skygreenxd <dubov_2005@mail.ru>
2026-04-06 16:53:03 +0300

    add prometheus metrics

---
+++ internal/infrastructure/metrics/prometheus.go
@@ -0,0 +1,34 @@
+package metrics
+
+import (
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	// сколько запросов пришло
+	HttpRequestsTotal = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_requests_total",
+			Help: "Total HTTP requests",
+		},
+		[]string{"method", "path", "status"},
+	)
+
+	// время ответа
+	HttpRequestDuration = promauto.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "http_request_duration_seconds",
+			Help:    "HTTP request duration",
+			Buckets: prometheus.DefBuckets,
+		},
+		[]string{"method", "path"},
+	)
+)
+
+func Handler() http.Handler {
+	return promhttp.Handler()
+}
