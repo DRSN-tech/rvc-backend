78e1f7390e05195c10d17a73d62b0c53c4ed0f62
skygreenxd <dubov_2005@mail.ru>
2026-04-06 16:54:08 +0300

    add new metrics router and use metrics middleware

--- internal/delivery/v1/http/router.go
+++ internal/delivery/v1/http/router.go
@@ -2,6 +2,7 @@ package http
 
 import (
 	_ "github.com/DRSN-tech/go-backend/docs" // Импорт сгенерированных файлов
+	"github.com/DRSN-tech/go-backend/internal/infrastructure/metrics"
 	"github.com/DRSN-tech/go-backend/internal/usecase"
 	"github.com/DRSN-tech/go-backend/pkg/logger"
 	"github.com/go-chi/chi/v5"
@@ -12,22 +13,24 @@ import (
 type Router struct {
 	router *chi.Mux
 	logger logger.Logger
+	prUC   usecase.ProductUC
 }
 
-func NewRouter(router *chi.Mux, logger logger.Logger) *Router {
-	return &Router{router: router, logger: logger}
+func NewRouter(router *chi.Mux, logger logger.Logger, prUC usecase.ProductUC) *Router {
+	return &Router{router: router, logger: logger, prUC: prUC}
 }
 
-func (r *Router) Init(prUC usecase.ProductUC) {
+func (r *Router) Init() {
 	r.router.Use(middleware.Logger)    // Пишет логи запросов в консоль
 	r.router.Use(middleware.Recoverer) // Не дает серверу упасть при панике
-	
+	r.router.Use(MetricsMiddleware)
+
 	r.router.Get("/swagger/*", httpSwagger.Handler(
 		httpSwagger.URL("http://localhost:8080/swagger/doc.json"), // ссылка на JSON
 	))
-	
+
 	r.router.Route("/api/v1", func(v1 chi.Router) {
-		prHandler := NewProductHandler(prUC, r.logger)
+		prHandler := NewProductHandler(r.prUC, r.logger)
 		registerProductRoutes(v1, prHandler)
 	})
 }
@@ -36,4 +39,11 @@ func registerProductRoutes(router chi.Router, prHandler *ProductHandler) {
 	router.Route("/products", func(pr chi.Router) {
 		pr.Post("/", prHandler.registerNewProduct)
 	})
+}
+
+func InitMetricsRouter() *chi.Mux {
+	r := chi.NewRouter()
+	r.Handle("/metrics", metrics.Handler())
+
+	return r
 }
