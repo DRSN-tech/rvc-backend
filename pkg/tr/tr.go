638922b95a008f0a4013c8cb3a41a09e3231e5a2
skygreenxd <dubov_2005@mail.ru>
2026-04-06 17:17:25 +0300

    add new type

--- pkg/tr/tr.go
+++ pkg/tr/tr.go
@@ -7,6 +7,10 @@ import (
 	"github.com/jackc/pgx/v5"
 )
 
+type contextKey string
+
+const TxKey contextKey = "tx"
+
 // TxFromCtx извлекает объект транзакции (pgx.Tx) из контекста
 func TxFromCtx(ctx context.Context) (pgx.Tx, error) {
 	txAny := ctx.Value("tx")
